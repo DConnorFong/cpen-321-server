ea7a1e97ff65c58e6e2a5ae9a898f7dbdc52c890
Connor Fong <connor_fong@hotmail.com>
2019-10-23 22:10:05 -0700

    (no ticket) updated navbar spacing issue, changed color style

--- mobile/src/components/Navbar/Navbar.tsx
+++ mobile/src/components/Navbar/Navbar.tsx
@@ -7,7 +7,12 @@ import {
     Text,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { navbarStyles } from '../../styles/navbar';
+import {
+    navbarStyles,
+    navbarBaseColor,
+    navbarActiveColor,
+    navbarHighlightColor,
+} from '../../styles/navbar';
 
 interface NavBarProps {
     OnPressNavButtonSearch: Function;
@@ -15,68 +20,164 @@ interface NavBarProps {
     OnPressNavButtonChat: Function;
 }
 
-interface NavBarState {}
+/*
+ * active: 0 - Group; 1 - Schedule; 2 - Messages; 3 - Settings
+ */
+interface NavBarState {
+    active: Number;
+}
+
 export default class Navbar extends Component<NavBarProps, NavBarState> {
+    constructor(props: NavBarProps) {
+        super(props);
+
+        this.handleSelectionGroup = this.handleSelectionGroup.bind(this);
+        this.handleSelectionSchedule = this.handleSelectionSchedule.bind(this);
+        this.handleSelectionMessages = this.handleSelectionMessages.bind(this);
+        this.handleSelectionSettings = this.handleSelectionSettings.bind(this);
+        this.renderSelectionColor = this.renderSelectionColor.bind(this);
+
+        this.state = {
+            active: 0,
+        };
+    }
+
     render() {
         return (
             <SafeAreaView style={navbarStyles.navbarContainer}>
                 <TouchableHighlight
-                    onPress={this.props.OnPressNavButtonSearch.bind(this)}
+                    onPress={this.handleSelectionGroup.bind(this)}
                     style={navbarStyles.navButton}
-                    underlayColor="rgba(255,255,255,0.4)"
+                    underlayColor={navbarHighlightColor}
                 >
                     <View style={navbarStyles.navButtonContainer}>
                         <View style={navbarStyles.navButtonIconContainer}>
                             <Ionicons
                                 name="ios-people"
-                                size={35}
-                                color="white"
+                                size={30}
+                                color={this.renderSelectionColor(0)}
                             />
                         </View>
                         <View style={navbarStyles.navButtonTextContainer}>
-                            <Text style={navbarStyles.navButtonText}>
+                            <Text
+                                style={[
+                                    navbarStyles.navButtonText,
+                                    { color: this.renderSelectionColor(0) },
+                                ]}
+                            >
                                 Group
                             </Text>
                         </View>
                     </View>
                 </TouchableHighlight>
                 <TouchableHighlight
-                    onPress={this.props.OnPressNavButtonSch.bind(this)}
+                    onPress={this.handleSelectionSchedule.bind(this)}
                     style={navbarStyles.navButton}
-                    underlayColor="rgba(255,255,255,0.4)"
+                    underlayColor={navbarHighlightColor}
                 >
                     <View style={navbarStyles.navButtonContainer}>
                         <View style={navbarStyles.navButtonIconContainer}>
                             <Ionicons
                                 name="ios-calendar"
-                                size={35}
-                                color="white"
+                                size={30}
+                                color={this.renderSelectionColor(1)}
                             />
                         </View>
                         <View style={navbarStyles.navButtonTextContainer}>
-                            <Text style={navbarStyles.navButtonText}>
+                            <Text
+                                style={[
+                                    navbarStyles.navButtonText,
+                                    { color: this.renderSelectionColor(1) },
+                                ]}
+                            >
                                 Schedule
                             </Text>
                         </View>
                     </View>
                 </TouchableHighlight>
                 <TouchableHighlight
-                    onPress={this.props.OnPressNavButtonChat.bind(this)}
+                    onPress={this.handleSelectionMessages.bind(this)}
                     style={navbarStyles.navButton}
-                    underlayColor="rgba(255,255,255,0.4)"
+                    underlayColor={navbarHighlightColor}
                 >
                     <View style={navbarStyles.navButtonContainer}>
                         <View style={navbarStyles.navButtonIconContainer}>
-                            <Ionicons name="ios-text" size={35} color="white" />
+                            <Ionicons
+                                name="ios-text"
+                                size={30}
+                                color={this.renderSelectionColor(2)}
+                            />
                         </View>
                         <View style={navbarStyles.navButtonTextContainer}>
-                            <Text style={navbarStyles.navButtonText}>
+                            <Text
+                                style={[
+                                    navbarStyles.navButtonText,
+                                    { color: this.renderSelectionColor(2) },
+                                ]}
+                            >
                                 Messages
+                            </Text>
+                        </View>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    onPress={this.handleSelectionSettings.bind(this)}
+                    style={navbarStyles.navButton}
+                    underlayColor={navbarHighlightColor}
+                >
+                    <View style={navbarStyles.navButtonContainer}>
+                        <View style={navbarStyles.navButtonIconContainer}>
+                            <Ionicons
+                                name="ios-settings"
+                                size={30}
+                                color={this.renderSelectionColor(3)}
+                            />
+                        </View>
+                        <View style={navbarStyles.navButtonTextContainer}>
+                            <Text
+                                style={[
+                                    navbarStyles.navButtonText,
+                                    { color: this.renderSelectionColor(3) },
+                                ]}
+                            >
+                                Settings
                             </Text>
                         </View>
                     </View>
                 </TouchableHighlight>
             </SafeAreaView>
         );
+    }
+
+    handleSelectionGroup() {
+        this.setState({
+            active: 0,
+        });
+        this.props.OnPressNavButtonSearch();
+    }
+
+    handleSelectionSchedule() {
+        this.setState({
+            active: 1,
+        });
+        this.props.OnPressNavButtonSch();
+    }
+
+    handleSelectionMessages() {
+        this.setState({
+            active: 2,
+        });
+        this.props.OnPressNavButtonChat();
+    }
+
+    handleSelectionSettings() {
+        this.setState({
+            active: 3,
+        });
+    }
+
+    renderSelectionColor(active: Number) {
+        if (active === this.state.active) return navbarActiveColor;
+        else return navbarBaseColor;
     }
 }
--- mobile/src/styles/navbar.ts
+++ mobile/src/styles/navbar.ts
@@ -1,40 +1,48 @@
 import { StyleSheet } from 'react-native';
 
+export const navbarBaseColor = '#444444';
+
+export const navbarActiveColor = '#947BD3';
+
+// equivalent to #947BD3 with transparency
+export const navbarHighlightColor = 'rgba(148,123,221,0.1)';
+
+const navbarBorderColor = '#AAAAAA';
+
 export const navbarStyles = StyleSheet.create({
     navbarContainer: {
         flex: 1,
         flexDirection: 'row',
-        backgroundColor: '#01a3a4',
+        backgroundColor: '#FFFFFF',
         justifyContent: 'center',
+        borderTopColor: navbarBorderColor,
+        borderTopWidth: 1,
     },
     navButtonContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingTop: 3,
     },
     navButton: {
         flex: 1,
         alignItems: 'center',
-    },
-    navButtonTitle: {
-        fontSize: 15,
-        color: 'white',
+        height: 50,
+        borderRadius: 15,
     },
     navButtonText: {
-        color: 'white',
         textAlign: 'center',
-        fontWeight: '700',
         fontSize: 11,
+        top: 5,
     },
     navButtonIconContainer: {
-        flex: 3,
         justifyContent: 'center',
         alignItems: 'center',
+        bottom: 2,
+        top: 2,
     },
     navButtonTextContainer: {
         flex: 1,
         justifyContent: 'center',
-        bottom: 7,
+        bottom: 10,
     },
 });
